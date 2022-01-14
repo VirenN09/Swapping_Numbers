8b0df83eef3d9c058f718dc342dc4493d7895776
VirenN09 <97726011+VirenN09@users.noreply.github.com>
2022-01-14 19:14:59 +0530

    Update code.c

--- code.c
+++ code.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-  int number1,number2,a;
+  int number1,number2,a; //declaring variables
   printf("enter the first number:");
   scanf("%d",&number1);
   printf("enter the second number:");
   scanf("%d",&number2);
-  a=number1;
-  number1=number2;
-  number2=a;
+  a=number1; //value of first number given to a
+  number1=number2; //value of first number given to second number
+  number2=a; //finally number2 has number1's value
   printf("first number swaped:%d",number1);
   printf("\n second number swaped:%d",number2);
   return 0;
